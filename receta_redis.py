9f6861558b27b4fc55c47c292d59add9afa26c88
Said-Pe <pecetosaid323@gmail.com>
2024-03-24 13:25:24 -0500

    Second Commit

--- receta_redis.py
+++ receta_redis.py
@@ -24,72 +24,73 @@ def add_recipe():
   print("Receta agregada exitosamente.")
 
 def update_recipe():
-  # Get recipe ID from user input
+
   recipe_id = input("Ingrese el ID de la receta que desea actualizar: ")
-  # Construct the recipe key
+
   recipe_key = f'recipe:{recipe_id}'
 
-  # Check if recipe exists using EXISTS
+
   if r.exists(recipe_key):
-    # Get user input for updates
+
     name = input("Nuevo nombre de la receta: ")
     ingredients = input("Nuevos ingredientes (separados por comas): ")
     steps = input("Nuevos pasos de la receta: ")
 
-    # Prepare updated recipe data dictionary
+    #Para actualizar una receta
     updated_recipe = {'name': name, 'ingredients': ingredients, 'steps': steps}
 
-    # Update recipe data using HMSET
-    r.hmset(recipe_key, updated_recipe)
+    r.hset(recipe_key, 'name', name)
+    r.hset(recipe_key, 'ingredients', ingredients)
+    r.hset(recipe_key, 'steps', steps)
     print("Receta actualizada exitosamente.")
   else:
     print("No se encontró ninguna receta con ese ID.")
 
 def delete_recipe():
-  # Get recipe ID from user input
+
   recipe_id = input("Ingrese el ID de la receta que desea eliminar: ")
-  # Construct the recipe key
+
   recipe_key = f'recipe:{recipe_id}'
 
-  # Check if recipe exists using EXISTS
+
   if r.exists(recipe_key):
-    # Delete the recipe using DEL
+
     r.delete(recipe_key)
     print("Receta eliminada exitosamente.")
   else:
     print("No se encontró ninguna receta con ese ID.")
 
 def view_recipes():
-  # Get all recipe keys using KEYS
+
   keys = r.keys('recipe:*')
-  # Check if any recipes are found
+
   if keys:
     for key in keys:
-      # Retrieve recipe details using HGETALL
+
       recipe = r.hgetall(key)
-      # Decode bytes and format output
+
       print(f"ID: {key.decode()}, Nombre: {recipe[b'name'].decode()}, Ingredientes: {recipe[b'ingredients'].decode()}, Pasos: {recipe[b'steps'].decode()}")
   else:
     print("No hay recetas disponibles.")
 
 def search_recipe():
-  # Get ingredient to search for
+
   ingredient = input("Ingrese un ingrediente para buscar recetas: ")
   # Get all recipe keys using KEYS
   keys = r.keys('recipe:*')
   found_recipes = []
   for key in keys:
-    # Retrieve recipe details using HGETALL
+
     recipe = r.hgetall(key)
     ingredients = recipe[b'ingredients'].decode()
-    # Search ingredient (case-insensitive)
+
     if ingredient.lower() in ingredients.lower():
       found_recipes.append(recipe)
 
-  # Check if any recipes were found
+
   if found_recipes:
     for recipe in found_recipes:
-      # Decode bytes and format output
+
       print(f"ID: {key.decode()}, Nombre: {recipe[b'name'].decode()}, Ingredientes: {recipe[b'ingredients'].decode()}, Pasos: {recipe[b'steps'].decode()}")
   else:
     print(f"No hay recetas que contengan {ingredient}.")
